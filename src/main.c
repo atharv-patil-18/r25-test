6a4491a27a5969492fce6ea797bc2f16d0de62b6
atharv-patil-18 <atharvcoding18@gmail.com>
2024-09-06 18:05:11 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,41 +1,23 @@
 #include <stdint.h>
-#include <stdio.h>
-#include <serial.h>
-#include <parsing.h>
+#include <math.h>
 
-int main(int argc, char** argv) {
-	char *port_name_1 = argv[1]; // SBUS 
-	char *port_name_2 = argv[2]; // Sabertooth1
+// Constants
+#define CHANNEL_MIN 0
+#define CHANNEL_MAX 2047
+#define PWM_MIN 0
+#define PWM_MAX 255
 
-	FILE *sbus; 
-	FILE *sabertooth;
+int interpolation(uint16_t channel) {
+    // Clamp channel value to be within the expected range
+    if (channel < CHANNEL_MIN) {
+        channel = CHANNEL_MIN;
+    } else if (channel > CHANNEL_MAX) {
+        channel = CHANNEL_MAX;
+    }
 
-	// to store sbus packets
-	uint8_t sbus_packet[15];
+    // Perform linear interpolation
+    int pwm = (int)(((float)(channel - CHANNEL_MIN) / (CHANNEL_MAX - CHANNEL_MIN)) * (PWM_MAX - PWM_MIN) + PWM_MIN);
 
-	// to store value of indiviual RC channel
-	uint16_t *channel;
-
-	// pwm value after interpolation 
-	int pwm;
-
-	// opening serial port for serial communication with Sabertooth and SBUS
-	sbus = open_file(port_name_1, "rb");
-	sabertooth = open_file(port_name_2, "w+");
-	
-	// read data from RC transmitter using sbus
-	read_SBUS(sbus_packet, uint8_t, 25, sbus);
-
-	// parsing sbus packet
-	channel = parse_buffer(sbus_packet);
-
-	// get pwm range for Sabertooth 1			 
-	pwm = interpolation(channel[0]);		//  write								
-							//  to
-	// write data to Sabertooth 1			//  sabertooth	
-	write_to_SB(sabertooth, "%d", pwm);		
-
-	// closing all serial port 
-	close_file(sbus);
-	close_file(sabertooth);
+    // Return the interpolated PWM value
+    return pwm;
 }
